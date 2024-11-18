1e8a90d75a8eae199774acc68919b650b2a922d0
ZainebPadilla <zainebt16@gmail.com>
2024-11-18 21:21:04 +0100

    ajout 7

--- script.js
+++ script.js
@@ -58,3 +58,70 @@ navbar.addEventListener("dblclick", function(){
 
     bootstrapLink.disabled = !bootstrapLink.disabled;
 });
+
+// fonctionalité 6
+var firstCard = document.querySelectorAll(".card")[0];
+var viewButton = firstCard.querySelector(".btn.btn-sm.btn-success");
+
+var cardImage = firstCard.querySelector(".card-img-top");
+var cardText = firstCard.querySelector(".card-text");
+
+
+viewButton.addEventListener("mouseover", function() {
+    // Réduire l'image
+    cardImage.style.width = "20%";
+    // zffacer le texte
+    cardText.classList.toggle("card-text");
+});
+
+viewButton.addEventListener("mouseout", function() {
+    // Rétablir la taille normale de l'image
+    cardImage.style.width = "100%";
+    // Réafficher le texte
+    cardText.classList.toggle("card-text");
+});
+
+// pour toutes les cards :
+var allCards = document.querySelectorAll(".card"); //retourne un array : il faut un for each
+allCards.forEach(function(card) {
+
+    var viewButton = card.querySelector(".btn.btn-sm.btn-success");
+
+    var cardImage = card.querySelector(".card-img-top");
+    var cardText = card.querySelector(".card-text");
+
+
+    viewButton.addEventListener("mouseover", function() {
+        // Réduire l'image
+        cardImage.style.width = "20%";
+        // zffacer le texte
+        cardText.classList.toggle("card-text");
+    });
+
+    viewButton.addEventListener("mouseout", function() {
+        // Rétablir la taille normale de l'image
+        cardImage.style.width = "100%";
+        // Réafficher le texte
+        cardText.classList.toggle("card-text");
+    });
+
+});
+
+
+// fonctionalité 7
+ var bannerElements = document.querySelector(".jumbotron.text-center");
+ var grisBtn = bannerElements.querySelector(".btn.btn-secondary.my-2");
+ console.log(grisBtn); //verif
+
+ var cardParent = document.querySelector(".album.py-5.bg-light");
+
+ grisBtn.addEventListener("click", function() { 
+
+    var allCards = cardParent.querySelectorAll(".card");
+
+     // Sélectionner la dernière card
+    var lastCard = allCards[allCards.length - 1];
+
+     // Déplacer la dernière card avant la première
+    cardParent.insertBefore(lastCard, allCards[0]);
+ });
